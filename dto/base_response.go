7d30fab567af9f3a413a28666273e074d3f051f2
Madhu Verma <madhuvermamit2k15@gmail.com>
2023-09-23 10:49:06 +0530

    updated base response

--- dto/base_response.go
+++ dto/base_response.go
@@ -1,6 +1,8 @@
 package dto
 
 type BaseResponse struct {
+	MsgID          string `json:"msg_id"`
+	Success        bool   `json:"success"`
 	AccessToken    string `json:"access_token"`
 	RefreshToken   string `json:"refresh_token"`
 	IsTokenRefresh bool   `json:"is_token_refreshed"`
